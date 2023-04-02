5bdabbc7c7bd3b2763cf527096094e73a4aff0a9
Munshid <muhammadmunshid200@gmail.com>
2023-04-02 20:29:41 +0530

    forget password,set password including otp backend added

--- routes/managerRoute.js
+++ routes/managerRoute.js
@@ -10,10 +10,11 @@ let Email;
 let Mobile;
 let Password;
 
+let forgetMobile;
+
 router.post('/signUp', async (req, res) => {
     try {
         let managerData = req.body
-        console.log(managerData);
         const { name, email, mobile, password } = req.body
         Name = name
         Email = email
@@ -21,8 +22,7 @@ router.post('/signUp', async (req, res) => {
         Password = password
         managerModel.findOne({ email: managerData.email }).then((user) => {
             if (user) {
-                // response.userExist = true
-                res.status(200).send({ exist: true })
+                res.status(200).send({ exist: true, message:'You are already signed' })
             } else {
                 // const accountSid = "AC8d467a2f7eafcfbf1c4d2adb93b59719";
                 // const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -55,7 +55,6 @@ router.post('/otp', async (req, res) => {
     try {
         let otpNumber = req.body.otp
         // if (otpCode == otpNumber) {
-        console.log(Name);
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(Password, salt)
         Password = hashedPassword
@@ -77,5 +76,78 @@ router.post('/otp', async (req, res) => {
         res.status(500).send({ error: true })
     }
 })
+
+router.post('/forgot', async (req, res) => {
+    try {
+        forgetMobile = req.body.mobile
+        managerModel.findOne({ mobile: forgetMobile }).then((user) => {
+            if (user) {
+                const accountSid = "AC8d467a2f7eafcfbf1c4d2adb93b59719";
+                const authToken = process.env.TWILIO_AUTH_TOKEN;
+                const client = new twilio(accountSid, authToken)
+                const mobile = '+91' + forgetMobile
+                const toPhoneNumber = mobile
+                client.messages
+                    .create({
+                        to: toPhoneNumber,
+                        from: '+15854605014',
+                        body: `Your OTP code is ${otpCode}`,
+                    })
+                    .then((message) => {
+                        console.log(message.sid)
+                        res.status(200).send({ success: true })
+                    })
+                    .catch((error) => console.log(error));
+            } else {
+                res.status(200).send({ noacc: true, message: 'You are not registered in this account' })
+            }
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: true })
+    }
+
+})
+
+router.post('/reset-otp', async (req, res) => {
+    try {
+        let otpNumber = req.body.otp
+        if (otpCode == otpNumber) {
+            res.status(200).send({ success: true, message: 'Your new password enter here...' })
+        } else {
+            console.log('error');
+            res.status(500).send({ error: true })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: true })
+    }
+})
+
+router.post('/reset-pswrd', async (req, res) => {
+    try {
+        let Password = req.body.password
+        let confirmPassword = req.body.confirmPassword
+        if (Password === confirmPassword) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(Password, salt)
+            Password = hashedPassword
+            managerModel.findOneAndUpdate({ mobile: forgetMobile }, {
+                $push: {
+                    password: Password
+                }
+            }).then(() => {
+                res.status(200).send({ success: true, message: 'Your reset password successfully' })
+            })
+        } else {
+            res.status(500).send({ message: 'Your password is not matched' })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: true })
+    }
+})
+
 
 module.exports = router
